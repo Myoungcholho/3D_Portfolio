3cdbf6bc93a43019c6efd012709c575eda0f8be9
Myoungcholho <mch473700@gmail.com>
2024-06-26 03:18:12 +0900

    명철호
    플레이어 이동

---
+++ Assets/Script/Component/MovingComponent.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovingComponent : MonoBehaviour
+{
+    [SerializeField]
+    private float walkSpeed = 2.0f;
+
+    [SerializeField]
+    private float runSpeed = 4.0f;
+
+    [SerializeField]
+    private float sensitivity = 10.0f;
+
+    [SerializeField]
+    private float deadZone = 0.001f;
+
+    private bool bCanMove = true;
+    private Animator animator;
+    private CharacterController controller;
+    
+    private Vector2 inputMove;
+    public Vector2 MoveValue { get => inputMove; }
+    private Vector2 currInputMove;
+    private bool bRun;
+    private Vector2 velocity;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+        // 1. PlayerInput 컴포넌트를 가져오기
+        // 2. PlayerInput 컴포넌트의 ActionMap 가져오기
+        // 3. ActionMap에서 Binding한 것 가져오기
+        // 4. Bind의 event에 메서드 연결하기
+        PlayerInput input = GetComponent<PlayerInput>();
+        InputActionMap actionMap = input.actions.FindActionMap("Player");
+
+        InputAction moveAction = actionMap.FindAction("Move");
+        moveAction.performed += Input_Move_Performed;
+        moveAction.canceled += Input_Move_Cancled;
+
+        InputAction runAction = actionMap.FindAction("Run");
+        runAction.started += Input_Run_Started;
+        runAction.canceled += Input_Run_Cancled;
+    }
+
+    private void Input_Move_Performed(InputAction.CallbackContext context)
+    {
+        inputMove = context.ReadValue<Vector2>();
+    }
+    
+    private void Input_Move_Cancled(InputAction.CallbackContext context) 
+    {
+        inputMove = Vector2.zero;
+    }
+
+    private void Input_Run_Started(InputAction.CallbackContext context)
+    {
+        bRun = true;
+    }
+
+    private void Input_Run_Cancled(InputAction.CallbackContext context)
+    {
+        bRun = false;
+    }
+
+    private void Update()
+    {
+        currInputMove = Vector2.SmoothDamp(currInputMove, inputMove, ref velocity, 1.0f / sensitivity);
+
+        if (bCanMove == false)
+            return;
+
+        Vector3 direction = Vector3.zero;
+
+        float speed = bRun ? runSpeed : walkSpeed;
+        if (currInputMove.magnitude > deadZone)
+        {
+            direction = (Vector3.right * currInputMove.x) + (Vector3.forward * currInputMove.y);
+            direction = direction.normalized * speed;
+        }
+        controller.Move(direction * Time.deltaTime);
+
+        animator.SetFloat("SpeedY", direction.magnitude);
+    }
+
+    private void OnGUI()
+    {
+        GUI.color = Color.red;
+        GUILayout.Label(inputMove.ToString());
+        //GUILayout.Label(currInputMove.ToString());
+    }
+}
